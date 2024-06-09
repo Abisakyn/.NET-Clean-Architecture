4f27d187f55c3b9c47e6d5388c68f56af3f94057
Abisagy Nafula <abisakyn@gmail.com>
2024-06-09 23:10:17 +0300

    Added Error Handling

--- BurberDinner.Api/Program.cs
+++ BurberDinner.Api/Program.cs
@@ -1,6 +1,6 @@
+using BurberDinner.Api.Filters;
 using BurberDinner.Application;
 using BurberDinner.Infrastructure;
-
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,18 +8,18 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+    // Uncomment the following line if you want to add error handling filter globally
+    // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
 }
 
-
-
 var app = builder.Build();
-
 {
     // Configure the HTTP request pipeline
+    // Uncomment the following line if you want to use custom middleware for error handling
+    // app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/errors"); // Ensure this matches your controller route
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
 }
-
-
